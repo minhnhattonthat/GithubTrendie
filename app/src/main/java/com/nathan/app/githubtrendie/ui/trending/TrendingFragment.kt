22c2b7a15df80e4fef85b14d35e377c13588ffc9
Nhat Ton <minhnhattonthat@gmail.com>
2020-01-19 17:31:52 +0800

    feat: add trending list layout

--- app/src/main/java/com/nathan/app/githubtrendie/ui/trending/TrendingFragment.kt
+++ app/src/main/java/com/nathan/app/githubtrendie/ui/trending/TrendingFragment.kt
@@ -1,12 +1,16 @@
 package com.nathan.app.githubtrendie.ui.trending
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.PopupMenu
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.nathan.app.githubtrendie.R
+
 
 class TrendingFragment : Fragment() {
 
@@ -16,11 +20,25 @@ class TrendingFragment : Fragment() {
 
     private lateinit var viewModel: TrendingViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.trending_fragment, container, false)
+        val view = inflater.inflate(R.layout.trending_fragment, container, false)
+
+        val moreIcon = view.findViewById<ImageView>(R.id.more_icon)
+        val popupMenu = PopupMenu(this.context!!, moreIcon)
+        popupMenu.inflate(R.menu.menu_trending)
+        moreIcon.setOnClickListener {
+            popupMenu.show()
+        }
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -29,4 +47,17 @@ class TrendingFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.name_menu_item -> {
+                // TODO: User clicked the name button
+                true
+            }
+            R.id.stars_menu_item -> {
+                // TODO: User clicked the stars button
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
