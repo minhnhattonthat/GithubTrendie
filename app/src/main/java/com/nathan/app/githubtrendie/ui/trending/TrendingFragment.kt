91622735f51bd8a325382607640e1d92aebd53fc
Nhat Ton <minhnhattonthat@gmail.com>
2020-01-21 01:22:11 +0800

    feat: connect viewmodel with view for Trending screen

--- app/src/main/java/com/nathan/app/githubtrendie/ui/trending/TrendingFragment.kt
+++ app/src/main/java/com/nathan/app/githubtrendie/ui/trending/TrendingFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.widget.Button
 import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nathan.app.githubtrendie.MainActivity
 import com.nathan.app.githubtrendie.R
+import com.nathan.app.githubtrendie.databinding.TrendingFragmentBinding
+import com.nathan.app.githubtrendie.di.ViewModelFactory
+import com.nathan.app.githubtrendie.widget.SimpleLoadingAdapter
 
 
 class TrendingFragment : Fragment() {
@@ -17,6 +23,8 @@ class TrendingFragment : Fragment() {
     companion object {
         fun newInstance() = TrendingFragment()
     }
+
+    private lateinit var binding: TrendingFragmentBinding
 
     private lateinit var viewModel: TrendingViewModel
 
@@ -29,22 +37,43 @@ class TrendingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.trending_fragment, container, false)
+        binding = TrendingFragmentBinding.inflate(inflater)
 
-        val moreIcon = view.findViewById<ImageView>(R.id.more_icon)
+        // For databinding with LiveData
+        binding.lifecycleOwner = this
+
+        binding.loadingList.adapter = SimpleLoadingAdapter(IntArray(10))
+
+        binding.repoList.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        val moreIcon = binding.moreIcon
         val popupMenu = PopupMenu(this.context!!, moreIcon)
         popupMenu.inflate(R.menu.menu_trending)
         moreIcon.setOnClickListener {
             popupMenu.show()
         }
 
-        return view
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(TrendingViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProviders.of(this, ViewModelFactory(activity as MainActivity))
+            .get(TrendingViewModel::class.java)
+
+        binding.offlineLayout.findViewById<Button>(R.id.retry_button)
+            .setOnClickListener(viewModel.retryClickListener)
+
+        binding.swipeLayout.setOnRefreshListener(viewModel.swipeRefreshListener)
+
+        binding.repoList.adapter = viewModel.repoAdapter
+
+        binding.viewModel = viewModel
+
+        viewModel.loading.observe(this, Observer { loading ->
+            binding.swipeLayout.isRefreshing = loading
+        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/com/nathan/app/githubtrendie/utils/DataBindingHelper.kt
+++ app/src/main/java/com/nathan/app/githubtrendie/utils/DataBindingHelper.kt
@@ -1,5 +1,6 @@
 package com.nathan.app.githubtrendie.utils
 
+import android.graphics.Color
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
@@ -7,9 +8,20 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("avatarUrl")
-fun avatarUrl(imageView: ImageView?, url: String?) {
-    Glide.with(imageView!!)
+fun avatarUrl(imageView: ImageView, url: String?) {
+    Glide.with(imageView)
         .load(url)
-        .apply(RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL))
+        .apply(
+            RequestOptions()
+                .centerCrop().circleCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+        )
         .into(imageView)
+}
+
+@BindingAdapter("tintColor")
+fun tintColor(imageView: ImageView, tintString: String?) {
+    if (tintString != null) {
+        imageView.setColorFilter(Color.parseColor(tintString))
+    }
 }
