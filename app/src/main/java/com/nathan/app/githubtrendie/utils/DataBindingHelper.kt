57e781b0d4cd3b96ae2d753bd5ff70d93e8b6ee5
Nhat Ton <minhnhattonthat@gmail.com>
2020-01-22 21:07:51 +0800

    fix: format number of stars & forks

--- app/src/main/java/com/nathan/app/githubtrendie/utils/DataBindingHelper.kt
+++ app/src/main/java/com/nathan/app/githubtrendie/utils/DataBindingHelper.kt
@@ -2,10 +2,12 @@ package com.nathan.app.githubtrendie.utils
 
 import android.graphics.Color
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import java.text.NumberFormat
 
 @BindingAdapter("avatarUrl")
 fun avatarUrl(imageView: ImageView, url: String?) {
@@ -24,4 +26,9 @@ fun tintColor(imageView: ImageView, tintString: String?) {
     if (tintString != null) {
         imageView.setColorFilter(Color.parseColor(tintString))
     }
+}
+
+@BindingAdapter("formattedNumber")
+fun formattedNumber(textView: TextView, number: Int) {
+    textView.text = NumberFormat.getInstance().format(number)
 }
